da3f864c97615f6f9253e7371b9c072e457032cd
soumya-junubala <133960866+soumya-junubala@users.noreply.github.com>
2025-03-28 18:02:34 -0700

    Update index.js
    Signed-off-by: soumya-junubala <133960866+soumya-junubala@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// Update to listen on all network interfaces (0.0.0.0)
+app.listen(app.get('port'), '0.0.0.0', function() {
+  console.log("Node app is running at http://0.0.0.0:" + app.get('port'));
+});
